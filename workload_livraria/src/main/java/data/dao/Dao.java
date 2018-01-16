596cea285838a3d8d295c4fc9393ccd9bc109de1
marco <marco@DESKTOP-IH601VQ>
2018-01-16 20:18:58 -0200

    issue 4, 3, 2 e 1

--- workload_livraria/src/main/java/data/dao/Dao.java
+++ workload_livraria/src/main/java/data/dao/Dao.java
@@ -1,5 +1,13 @@
 package data.dao;
 
-public interface Dao {
+public interface Dao<modelType> {
+	
+	ModelType create(ModelType modelo);
+	
+	ModelType recuperar(long id);
+	
+	ModelType update(long id, String chave, String valor);
+	
+	ModelType delete(ModelType modelo)	
 
 }
